387fefbfa3f16c3ac5f1b991ef0ebd5151ea2062
Tzu-Heng, Huang <35376020+zihengh1@users.noreply.github.com>
2018-12-24 16:31:08 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -3,12 +3,12 @@
 OTA="$(grep -Po 'OTA: \K.*' /home/pi/Local/env.txt)"
 
 sleep 5
-[ -f /home/pi/airbox-pi/PiM25.py ] && {
-    /usr/bin/sudo git -C /home/pi/airbox-pi fetch origin
-    /usr/bin/sudo git -C /home/pi/airbox-pi reset --hard origin/master
-    /usr/bin/sudo nohup python -u /home/pi/airbox-pi/PiM25.py &
+[ -f /home/pi/PiM25/PiM25.py ] && {
+    /usr/bin/sudo git -C /home/pi/PiM25 fetch origin
+    /usr/bin/sudo git -C /home/pi/PiM25 reset --hard origin/sinica
+    /usr/bin/sudo nohup python -u /home/pi/PiM25/PiM25.py &
 } || {
-    /usr/bin/sudo git clone OTA /home/pi/airbox-pi
-    /usr/bin/sudo nohup python -u /home/pi/airbox-pi/PiM25.py &
+    /usr/bin/sudo git clone OTA /home/pi/PiM25
+    /usr/bin/sudo nohup python -u /home/pi/PiM25/PiM25.py &
 }
 
