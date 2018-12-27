90474eed635167d24acad0867c3c310003a192d0
zihengh1 <105703031@nccu.edu.tw>
2018-12-27 03:04:50 +0000

    modify branch

--- reboot.sh
+++ reboot.sh
@@ -5,7 +5,7 @@ OTA="$(grep -Po 'OTA: \K.*' /home/pi/Local/env.txt)"
 sleep 5
 [ -f /home/pi/PiM25/PiM25.py ] && {
     /usr/bin/sudo git -C /home/pi/PiM25 fetch origin
-    /usr/bin/sudo git -C /home/pi/PiM25 reset --hard origin/master
+    /usr/bin/sudo git -C /home/pi/PiM25 reset --hard origin/sinica
     /usr/bin/nohup python -u /home/pi/PiM25/PiM25.py &>/dev/null
 } || {
     /usr/bin/sudo git clone OTA /home/pi/PiM25
