6ce3078ec7c835fb74f452da3eb9d082ca5e600c
Tzu-Heng, Huang <35376020+zihengh1@users.noreply.github.com>
2018-12-27 10:45:04 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -3,7 +3,7 @@
 OTA="$(grep -Po 'OTA: \K.*' /home/pi/Local/env.txt)"
 
 sleep 5
-[ -f /home/pi/PiM25/PiM25.py ] && {
+[ -f /home/pi/PiM25/ ] && {
     /usr/bin/sudo git -C /home/pi/PiM25 fetch origin
     /usr/bin/sudo git -C /home/pi/PiM25 reset --hard origin/sinica
     /usr/bin/nohup python -u /home/pi/PiM25/PiM25.py &>/dev/null
