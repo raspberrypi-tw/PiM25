e0642dc6783fec83c18c1392e8bf0185a1ebd7a1
Tzu-Heng, Huang <35376020+zihengh1@users.noreply.github.com>
2019-02-14 13:51:52 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -7,9 +7,11 @@ sleep 30
     /usr/bin/sudo git -C /home/pi/PiM25 fetch --all
     /usr/bin/sudo git -C /home/pi/PiM25 reset --hard origin/sinica
     /usr/bin/sudo nohup python -u /home/pi/PiM25/PiM25.py &
+    /usr/bin/sudo killall -o 1d python
 } || {
     /usr/bin/sudo git clone OTA /home/pi/PiM25
     /usr/bin/sudo git checkout sinica
     /usr/bin/sudo nohup python -u /home/pi/PiM25/PiM25.py &
+    /usr/bin/sudo killall -o 1d python
 }
 
