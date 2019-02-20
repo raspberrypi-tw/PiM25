80e77c16d70983fec751d8567385c00a2d684109
zihengh1 <105703031@nccu.edu.tw>
2019-02-20 01:50:51 +0000

    update OTA

--- reboot.sh
+++ reboot.sh
@@ -6,12 +6,8 @@ sleep 30
 [ -f /home/pi/PiM25/PiM25.py ] && {
     /usr/bin/sudo git -C /home/pi/PiM25 fetch --all
     /usr/bin/sudo git -C /home/pi/PiM25 reset --hard origin/sinica
-    # /usr/bin/sudo nohup python -u /home/pi/PiM25/PiM25.py &
-    # /usr/bin/sudo killall -o 1d python &
 } || {
     /usr/bin/sudo git clone OTA /home/pi/PiM25
     /usr/bin/sudo git checkout sinica
-    # /usr/bin/sudo nohup python -u /home/pi/PiM25/PiM25.py &
-    # /usr/bin/sudo killall -o 1d python &
 }
 
